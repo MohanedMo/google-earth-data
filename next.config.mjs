487e4ccedeab95cc1779b69e612e9158dfa4d5cb
Mohaned Mohamed <mmhnd951@gmail.com>
2026-08-30 09:07:27 +0300

    feat: add leaflet dependencies and configure earth engine as an external package in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['@google/earthengine'],
+  },
+};
 
 export default nextConfig;
